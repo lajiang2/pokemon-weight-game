bde75147ee6d96d6edf2ec9eec233aac282b5018
Lawrence Jiang <lawrence@Lawrences-iMac.local>
2019-03-03 05:36:42 -0800

    Updates

---
+++ precache-manifest.0798e9dc37993a79fa78234fe540b31c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68eb126ddc5486b467d3",
+    "url": "/pokemon-weight-game/static/js/runtime~main.68eb126d.js"
+  },
+  {
+    "revision": "3ebffd957619931d24d2",
+    "url": "/pokemon-weight-game/static/js/main.3ebffd95.chunk.js"
+  },
+  {
+    "revision": "9485d122fde2a4a86e59",
+    "url": "/pokemon-weight-game/static/js/2.9485d122.chunk.js"
+  },
+  {
+    "revision": "3ebffd957619931d24d2",
+    "url": "/pokemon-weight-game/static/css/main.e20996b3.chunk.css"
+  },
+  {
+    "revision": "48cae261e368b6ead66b0b7a681e3074",
+    "url": "/pokemon-weight-game/index.html"
+  }
+];
